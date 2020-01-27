0d02524fa3639d42f839ffa76bfab91b255c9e7e
Zodiac-Onnillon <57006351+Zodiac-Onnillon@users.noreply.github.com>
2020-01-27 17:09:45 +0100

    Revert modbus fix (#12)
    JIRA : EQ-907 EQ-913
    
    Correction modbus timer event.
    
    Signed-off-by: Onnillon Antoine antoine.onnillon@zodiac.com

--- components/freemodbus/port/portevent_m.c
+++ components/freemodbus/port/portevent_m.c
@@ -121,16 +121,14 @@ xMBMasterPortEventGet( eMBMasterEventType * eEvent)
             MB_EVENT_POLL_MASK,     // The bits within the event group to wait for.
             pdTRUE,                 // Masked bits should be cleared before returning.
             pdFALSE,                // Don't wait for both bits, either bit will do.
-            3100);        // Wait forever for either bit to be set. TODO EQ-907
+            portMAX_DELAY);        // Wait forever for either bit to be set.
 
     // Check if poll event is correct
     if (MB_PORT_CHECK_EVENT(uxBits, MB_EVENT_POLL_MASK)) {
         *eEvent = (eMBMasterEventType)(uxBits & MB_EVENT_POLL_MASK);
         xEventHappened = TRUE;
     } else {
-        vMBMasterSetErrorType(EV_ERROR_RESPOND_TIMEOUT);
-        xMBMasterPortEventPost(EV_MASTER_ERROR_PROCESS);
-        //ESP_LOGE(MB_PORT_TAG,"%s: Incorrect event triggered.", __func__); TODO EQ-907
+        ESP_LOGE(MB_PORT_TAG,"%s: Incorrect event triggered.", __func__);
         xEventHappened = FALSE;
     }
     return xEventHappened;
